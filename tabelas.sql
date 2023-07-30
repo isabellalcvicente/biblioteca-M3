e5a844b610c0feb58c5fb5bb1367ce851c785301
isabellalcvicente <isabellavicente@ufpr.br>
2023-07-30 17:53:57 -0300

    criado tabelas generos, editoras, livros e emprestimos no SQL

--- tabelas.sql
+++ tabelas.sql
@@ -43,3 +43,45 @@ CREATE TABLE if not exists `autores` (
     `id_pessoa` INT,
 	 FOREIGN KEY (id_pessoa) REFERENCES pessoas(id_pessoa) ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+CREATE TABLE if not exists `generos` (
+  `id_genero` int PRIMARY KEY  NOT NULL AUTO_INCREMENT,
+  `genero` enum ('acao', 'aventura ', 'suspense', 'drama', 'terror', 'juvenil', 'teen')
+);
+
+CREATE TABLE if not exists `editoras` (
+    `id_editora` INT PRIMARY KEY  NOT NULL AUTO_INCREMENT ,
+    `empresa` VARCHAR(100),
+    `cnpj` INT,
+    `telefone` INT,
+    `email` VARCHAR(100)
+);
+
+CREATE TABLE if not exists `livros` (
+    `id_livro` INT PRIMARY KEY  NOT NULL AUTO_INCREMENT ,
+    `titulo` VARCHAR(100),
+    `ano` DATE,
+    `qtd_paginas` INT,
+    `ISBN` INT,
+    `idioma` VARCHAR(2),
+    `disponibilidade` BIT NOT NULL,
+    `id_autor` INT,
+	FOREIGN KEY (id_autor) REFERENCES autores(id_autor) ON UPDATE CASCADE ON DELETE CASCADE,
+    `id_genero` INT,
+	FOREIGN KEY (id_genero) REFERENCES generos(id_genero) ON UPDATE CASCADE ON DELETE CASCADE,
+    `id_editora` INT,
+	FOREIGN KEY (id_editora) REFERENCES editoras(id_editora) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+CREATE TABLE if not exists `emprestimos` (
+  `id_emprestimo` int PRIMARY KEY  NOT NULL AUTO_INCREMENT ,
+  `data_emprestimo` timestamp,
+  `data_devolucao` date,
+  `id_usuario` int,
+  FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario) ON UPDATE CASCADE ON DELETE CASCADE,
+  `id_livro` int,
+  FOREIGN KEY (id_livro) REFERENCES livros(id_livro) ON UPDATE CASCADE ON DELETE CASCADE,
+  `id_funcionario` int,
+  FOREIGN KEY (id_funcionario) REFERENCES funcionarios(id_funcionario) ON UPDATE CASCADE ON DELETE CASCADE
+);
